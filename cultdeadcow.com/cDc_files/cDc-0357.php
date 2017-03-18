010d4f49d45f1c6e2ef0c521f0217b56e1673b80
OPSXCQ <opsxcq@strm.sh>
2017-03-18 19:32:09 +0000

    update

---
+++ cultdeadcow.com/cDc_files/cDc-0357.php
@@ -0,0 +1,122 @@
+<html><title>cDc paramedia: texXxt #357</title><body bgcolor="#0000a8" text="#cfcfcf"><pre>
+                                    _
+                                   | \
+                                   |  \
+                                   | | \
+                            __     | |\ \             __
+      _____________       _/_/     | | \ \          _/_/     _____________
+     |  ___________     _/_/       | |  \ \       _/_/       ___________  |
+     | |              _/_/_____    | |   &gt; &gt;    _/_/_____               | |
+     | |             /________/    | |  / /    /________/               | |
+     | |                           | | / /                              | |
+     | |                           | |/ /                               | |
+     | |                           | | /                                | |
+     | |                           |  /                                 | |
+     | |                           |_/                                  | |
+     | |                                                                | |
+     | |      c   o   m   m   u   n   i   c   a   t   i   o   n   s     | |
+     | |________________________________________________________________| |
+     |____________________________________________________________________|
+&nbsp;
+  ...presents...                 Flag Rant
+                                                         by The Nightstalker
+                                                         7/15/1998-#357
+&nbsp;
+             __///////\ -cDc- CULT OF THE DEAD COW -cDc- /\\\\\\\__
+               \\\\\\\/  Everything You Need Since 1986  \///////
+  ___    _   _    ___     _   _    ___       _   _      ___    _   _      ___
+ |___heal_the_sick___raise_the_dead___cleanse_the_lepers___cast_out_demons___|
+&nbsp;</PRE>
+
+<P><IMG
+SRC="http://www.salonmag.com/comics/tomo/1998/06/src/15tomo.gif"
+ALT="Snarky Tom Tomorrow cartoon about the Anti Flag Burning Amendment."
+WIDTH=580 HEIGHT=501 ALIGN=bottom
+naturalsizeflag="3"><BR><A HREF="http://www.well.com/user/tomorrow">This
+Modern World</A> is copyright 1998 by Tom Tomorrow.</P>
+
+<P>
+<HR>
+</P>
+
+<H1><CENTER>"Desecrating"(?) The Flag</CENTER></H1>
+
+<H3>I'm getting sick and tired of all these "Patriots" who fulminate
+about the dozen-odd flag burnings a year, and how The Precious Symbol
+Of Our Country NEEDS the protection of a Constitutional Amendment!
+</H3>
+
+<H1><CENTER>Sod Off!</CENTER></H1>
+
+<P>You want to know what the real desecration of the flag is? It's
+that there are veterans of WW II, Korea, Viet-Nam, et al, who are
+homeless. These men and women who volunteered to offer "that last,
+full measure of devotion" in the defense of their country, are
+reduced to sleeping on the streets. You over-fed, knee-jerk patriots
+cross the fucking street to avoid that ragged, smelly individual
+pleading for spare change. You don't care that he helped liberate
+South Korea inch by inch, under unspeakable hardships, or that he
+answered a call to fight for freedom in Viet-Nam, or that he got
+dosed by nerve gas in Iraq. No, you fat fuckers don't give a rat's
+ass about him, but you all PISS yourselves over the thought that some
+bozo might burn a flag and that the Federal Government needs to come
+down on him with both jackbooted feet.</P>
+
+<P>So SCREW THE FLAG and WIPE MY ASS WITH IT! As long as there are
+veterans who don't have a safe place to sleep, food to eat, and the
+medical, physical and psychiatric care they deserve, then the flag is
+being desecrated far worse than anyone could do with a can of naptha
+and a Zippo!</P>
+
+<P>The Government has always considered veterans as a disposable
+commodity. Use them and then throw them away. From the very first,
+the surviving veteran has always been the first to be screwed by the
+Feds, and the last to be succored. Even today, the bullshit from the
+Feds about Gulf War Syndrome is sickening. These men and women are
+sick, they are dying, they are giving birth to seriously deformed
+children. This is not all in their minds. The Government just wants
+to weasel out of paying for their care and treatment.</P>
+
+<P>That the surviving veterans of America's wars do not take up arms
+and march on D.C., DEMANDING redress of their grievances amazes me.
+</P>
+
+<P>Well, not really. They tried that once before, right after World
+War I. Something called the "Bonus March." When they reached
+Washington, D.C., they wound up being FIRED UPON by the same Army
+they once served in, on the direct orders of the President.</P>
+
+<P>One day, though, the Hired Help in the White House, on the Hill
+and in the Pentagon, will be called forth (hopefully at bayonet
+point) to answer for their sins of omission and commission.</P>
+
+<P>And then, perhaps, the Government-Sanctioned Desecration of the
+Flag will cease.</P>
+
+<P>Don't hold your breath waiting for it to happen, though.</P>
+
+<PRE>&nbsp;
+&nbsp;
+    .-.                             _   _                             .-.
+   /   \           .-.             ((___))             .-.           /   \
+  /.ooM \         /   \       .-.  [ x x ]  .-.       /   \         /.ooM \
+-/-------\-------/-----\-----/---\--\   /--/---\-----/-----\-------/-------\-
+/lucky  13\     /       \   /     `-(' ')-'     \   /       \     /lucky  13\
+           \   /         `-'         (U)         `-'         \   /
+            `-'              the original e-zine              `-'    _
+      Oooo                    eastside westside                     / )   __
+ /)(\ (   \                       WORLDWIDE                        /  (  /  \
+ \__/  )  /  Copyright (c) 1998 cDc communications and the author. \   ) \)(/
+       (_/     CULT OF THE DEAD COW is a registered trademark of    oooO
+          cDc communications, PO Box 53011, Lubbock, TX, 79453, USA.      _
+  oooO              All rights reserved.  Edited by Omega           __   ( \
+ /   ) /)(\                                                        /  \  )  \
+ \  (  \__/       Save yourself!  Go outside!  Do something!       \)(/ (   /
+  \_)                     xXx   BOW to the COW   xXx                    Oooo 
+&nbsp;
+                          <A HREF="../index.html">http://www.cultdeadcow.com</A></PRE>
+
+<P>&nbsp;</P>
+</BODY>
+</HTML>
+
